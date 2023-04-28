08967ffcc5f74d3e232e24a798171cee3f8e46f8
Roma <119023868+DoranaX@users.noreply.github.com>
2023-04-28 17:17:41 +0700

    Add files via upload

---
+++ Решатель 3000/WindowsFormsApplication7/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WindowsFormsApplication7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butDiagnosis = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.labExist = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // butDiagnosis
+            // 
+            this.butDiagnosis.Location = new System.Drawing.Point(357, 329);
+            this.butDiagnosis.Name = "butDiagnosis";
+            this.butDiagnosis.Size = new System.Drawing.Size(113, 24);
+            this.butDiagnosis.TabIndex = 1;
+            this.butDiagnosis.Text = "Тыкай";
+            this.butDiagnosis.UseVisualStyleBackColor = true;
+            this.butDiagnosis.Click += new System.EventHandler(this.butDiagnosis_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(28, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "A = 0 см";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(28, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "B = 0 см";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(28, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "C = 0 см";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(144, 4);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(347, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.LargeChange = 1;
+            this.trackBar2.Location = new System.Drawing.Point(144, 55);
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(347, 45);
+            this.trackBar2.TabIndex = 6;
+            this.trackBar2.Value = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.LargeChange = 1;
+            this.trackBar3.Location = new System.Drawing.Point(145, 101);
+            this.trackBar3.Minimum = 1;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(346, 45);
+            this.trackBar3.TabIndex = 7;
+            this.trackBar3.Value = 1;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // labExist
+            // 
+            this.labExist.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labExist.Location = new System.Drawing.Point(318, 136);
+            this.labExist.Name = "labExist";
+            this.labExist.Size = new System.Drawing.Size(184, 98);
+            this.labExist.TabIndex = 8;
+            this.labExist.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(21, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 178);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(262, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(116, 220);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "label6";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 136);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 240);
+            this.panel1.TabIndex = 13;
+            this.panel1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 376);
+            this.Controls.Add(this.labExist);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.butDiagnosis);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Решатель 3000";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butDiagnosis;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label labExist;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel1;
+
+    }
+}
+
---
+++ Решатель 3000/WindowsFormsApplication7/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace WindowsFormsApplication7
+{
+    public partial class Form1 : Form
+    {
+        int a, b, c, max,k1,k2;
+        string info;
+        double S,p,Hight;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void butDiagnosis_Click(object sender, EventArgs e)
+        {
+            if ((a + b > c) && (a + c> b)&& (b + c > a) )
+                test();
+            else
+                {
+                MessageBox.Show("Треугольника не существует");
+                labExist.Visible = false;
+                panel1.Visible = false;
+                return;
+            }
+        }
+        private void test()
+        {
+                pictureBox1.Refresh();
+                info = "Треугольник существует";
+                #region поиск max
+                max = (a > b) ? a : b;
+                max = (max > c) ? max : c;
+                if (a == max)
+                {
+                    if (c < b)
+                    {
+                        k1 = c;
+                        k2 = b;
+                    }
+                    else
+                    {
+                        k1 = b;
+                        k2 = c;
+                    }
+                }
+                else if (b == max)
+                {
+                    if (a < c)
+                    {
+                        k1 = a;
+                        k2 = c;
+                    }
+                    else
+                    {
+                        k1 = c;
+                        k2 = a;
+                    }
+                    
+                }
+                else
+                {
+                    if (a < b)
+                    {
+                        k1 = a;
+                        k2 = b;
+                    }
+                    else
+                    {
+                        k1 = b;
+                        k2 = a;
+                    }
+                    
+                }
+                #endregion
+                if (max * max == k1 * k1 + k2 * k2)
+                    info = info + "\n Он прямоугольный ";
+                else if (max * max > k1 * k1 + k2 * k2)
+                    info = info + "\n Он тупоугольный ";
+                else info = info + "\n Он остроугольный ";
+                if ((k1 == k2) || (k1 == max) || (k2 == max))
+                    info = info + "и равнобедренный";
+                else if ((k1 != k2) && (k1 != max) && (k2 != max))
+                    info = info + "и разносторонний";
+                else if ((k1 == k2) && (k1 == max))
+                    info = info + "и равносторонний";
+
+                label4.Text = k1 + " см";
+                label5.Text = k2 + " см";
+                label6.Text = max + " см";
+                
+                height();
+        }
+
+        private void height()// не нужно но оставь
+        {
+            p = (double)(max+ k1 + k2) / 2;
+            S = Math.Sqrt(p * (p - k1) * (p - k2) * (p - max));
+            Hight = 2 * S / max;
+            Construction();
+        }
+        private void Construction()
+        {
+            panel1.Visible = true;
+            labExist.Visible = true;
+            
+
+            int intLength = 200;
+            double sin = (2 * S) / (max * k1);
+            double cos = Math.Cos(Math.Asin(sin));
+            double scale = intLength / max;
+
+            Pen MyPen = new Pen(Color.Gold, 4);
+            Bitmap btmBack = new Bitmap(250, 280);      //изображение
+            Graphics grBack = Graphics.FromImage(btmBack); //лучше объявить заранее глобально.
+            pictureBox1.BackgroundImage = btmBack;
+
+            int x1 = 0;
+            int y1 = 175;
+            int x2 = x1 + intLength;
+            int y2 = y1;
+            grBack.DrawLine(MyPen, x1, y1, x2, y2);
+
+            x2 = (int)(x1 + (k1 * cos * scale));
+            y2 = (int)(y1 - (k1 * sin * scale));
+            grBack.DrawLine(MyPen, x1, y1, x2, y2);
+
+            label4.Location = new Point((x2 - x1) / 2, y1-(y1 - y2) / 2);
+
+            x2 = x1 + intLength;
+            y2 = y1;
+            x1 = (int)(x1 + (k1 * cos * scale));
+            y1 = (int)(y1 - (k1 * sin * scale));
+            grBack.DrawLine(MyPen, x1, y1, x2, y2);
+            label5.Location = new Point(x1 + (x2 - x1) / 2, 175- (y2 - y1) / 2);
+
+            info += "\nПлощадь = " + Math.Round(S, 2)  + "\nПолупериметр = " + Math.Round(p, 2) + "\nВысота = " + Math.Round(Hight, 2);
+            labExist.Text = info;
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text ="A = "+ trackBar1.Value + " см";
+            a = trackBar1.Value;
+        }
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            label2.Text = "B = " + trackBar2.Value + " см";
+            b = trackBar2.Value;
+        }
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            label3.Text = "C = " + trackBar3.Value + " см";
+            c = trackBar3.Value;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "A = " + 1 + " см"; a = 1;
+            label2.Text = "B = " + 1 + " см"; b = 1;
+            label3.Text = "C = " + 1 + " см"; c = 1;
+        }
+
+    }
+}
